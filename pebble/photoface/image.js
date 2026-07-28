ab366acd008733948bb63ee737a637af2c112b50
Ted Presley <tapresle92@gmail.com>
2026-07-28 00:14:17 -0400

    Test image because this is being a pain

--- pebble/photoface/image.js
+++ pebble/photoface/image.js
@@ -5,216 +5,70 @@ let imageBytes = null;
 
 
 document
-    .getElementById("photoPicker")
-    .addEventListener(
-        "change",
-        loadPhoto
-    );
-
-
-function loadPhoto(event) {
-    const file =
-        event.target.files[0];
-
-    if (!file)
+.getElementById("send")
+.onclick = function()
+{
+    if(!imageBytes)
+    {
+        alert("No image");
         return;
+    }
 
 
-    const reader =
-        new FileReader();
-
-
-    reader.onload = function (e) {
-        const img =
-            new Image();
-
-
-        img.onload = function () {
-            processImage(img);
-        };
-
-
-        img.src =
-            e.target.result;
-    };
-
-
-    reader.readAsDataURL(file);
-}
-
-
-document
-    .getElementById("send")
-    .onclick = function () {
-
-        if (!imageBytes) {
-            alert(
-                "Select an image first"
-            );
-
-            return;
-        }
-
-
-        let encoded =
-            JSON.stringify(
-                Array.from(imageBytes)
-            );
-
-
-        document.location =
-            "pebblejs://close#" +
-            encodeURIComponent(
-                encoded
-            );
-    };
-
-
-
-function processImage(img) {
-
-    const canvas =
-        document.getElementById(
-            "preview"
+    let encoded =
+        JSON.stringify(
+            Array.from(imageBytes)
         );
 
 
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
+    document.location =
+        "pebblejs://close#" +
+        encodeURIComponent(encoded);
+};
 
 
-    const ctx =
-        canvas.getContext("2d");
 
-
-    ctx.clearRect(
-        0,
-        0,
-        WIDTH,
-        HEIGHT
-    );
-
-
-    // Crop image to exactly 144x168
-    const scale =
-        Math.max(
-            WIDTH / img.width,
-            HEIGHT / img.height
-        );
-
-
-    const newWidth =
-        img.width * scale;
-
-
-    const newHeight =
-        img.height * scale;
-
-
-    const x =
-        (WIDTH - newWidth) / 2;
-
-
-    const y =
-        (HEIGHT - newHeight) / 2;
-
-
-    ctx.drawImage(
-        img,
-        x,
-        y,
-        newWidth,
-        newHeight
-    );
-
-
-    const data =
-        ctx.getImageData(
-            0,
-            0,
-            WIDTH,
-            HEIGHT
-        );
-
-
+function processImage(img)
+{
     imageBytes =
-        convertToPebble(data);
+        createTestImage();
 
 
     console.log(
-        "Image bytes:",
+        "Test image bytes:",
         imageBytes.length
     );
 }
 
 
 
-function convertToPebble(image) {
-
-    const pixels =
-        image.data;
-
-
-    const output =
+function createTestImage()
+{
+    let output =
         new Uint8Array(
-            WIDTH * HEIGHT
-        );
+            WIDTH * HEIGHT);
 
 
-    let offset = 0;
+    for(let y = 0; y < HEIGHT; y++)
+    {
+        for(let x = 0; x < WIDTH; x++)
+        {
+            let index =
+                (y * WIDTH) + x;
 
 
-    for (
-        let i = 0;
-        i < pixels.length;
-        i += 4
-    ) {
-
-        const r =
-            pixels[i];
+            // White background
+            output[index] = 0xFF;
 
 
-        const g =
-            pixels[i + 1];
-
-
-        const b =
-            pixels[i + 2];
-
-
-        output[offset++] =
-            packPixel(
-                r,
-                g,
-                b
-            );
+            // Red square top left
+            if(x < 30 && y < 30)
+            {
+                output[index] = 0xE0;
+            }
+        }
     }
 
 
     return output;
-}
-
-
-
-// Pebble Time 2 GBitmapFormat8Bit
-// RRRGGGBB
-function packPixel(r, g, b) {
-
-    const red =
-        (r >> 5) & 0x07;
-
-
-    const green =
-        (g >> 5) & 0x07;
-
-
-    const blue =
-        (b >> 6) & 0x03;
-
-
-    return (
-        (red << 5) |
-        (green << 2) |
-        blue
-    );
 }
